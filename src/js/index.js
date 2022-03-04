cfca6446bba2a71abfaa9124f97dd8b0444f93bd
rameskum <rameskum.ms@outlook.com>
2022-03-04 16:02:08 +0530

    - adding homepage - change theme change duration to 5 sec - starting index to be 1 as first is the default - reordering theme sequence - change idx calculation logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,15 @@
 console.log("👋🏼 from Ramesh!");
 
-let themes = ["theme-red", "theme-grey", "theme-pink", "theme-grape", "theme-violet",
+let themes = ["theme-red", "theme-pink", "theme-grape", "theme-violet",
     "theme-indigo", "theme-blue", "theme-cyan", "theme-teal", "theme-green",
-    "theme-lime", "theme-yellow", "theme-orange"];
+    "theme-lime", "theme-yellow", "theme-orange", "theme-grey"];
 
 let bodyEl = document.querySelector("body");
 let currentTheme = bodyEl.className;
-let idx = 0;
+let idx = 1;
 
 setInterval(() => {
     bodyEl.classList.remove(currentTheme);
     bodyEl.className = themes[idx];
-    idx++;
-    if (idx > themes.length) {
-        idx = idx % themes.length;
-    }
-}, 10000);
+    idx = (idx + 1) % themes.length;
+}, 5000);
