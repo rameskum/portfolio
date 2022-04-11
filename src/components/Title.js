f6bf69b8ea56bfd5b41441e6be62f81819289caf
rameskum <rameskum.ms@outlook.com>
2022-04-11 17:47:56 +0530

    update title to support description

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,13 @@
 import React from "react"
 
-const Title = ({title, className}) => {
+const Title = ({title, sectionDesc = null, className}) => {
     return (
         <div className={className || 'section-title'}>
             <h2>{title || "default title"}</h2>
             <div className="underline"/>
+            {
+                sectionDesc && <p>{sectionDesc}</p>
+            }
         </div>
     )
 }
