0d8237682b04d8232387c174214f6472da7b2248
rameskum <rameskum.ms@outlook.com>
2022-04-12 12:38:32 +0530

    404 redesign

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,9 +3,12 @@ import "../styles/partials/_not_found.scss"
 function NotFound() {
 
     return (
-        <div className="notFound">
-            <h1>404</h1>
-        </div>
+        <>
+            <div className="section single-page">
+                <div className="number">404</div>
+                <div className="text"><span>Ooops...</span><br/>page not found</div>
+            </div>
+        </>
     )
 }
 
