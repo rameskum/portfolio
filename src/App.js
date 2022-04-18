86faad8ce09c9bbbde7a75fecb58bae536a8be32
rameskum <rameskum.ms@outlook.com>
2022-04-18 15:53:15 +0530

    scroll to top on page change smooth scrolling

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import HomePage from './pages/HomePage';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import ProjectsPage from './pages/ProjectsPage';
 import NotFound from './pages/NotFound';
 import Footer from './components/Footer';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import Seo from './components/Seo';
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-	const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-	const toggleSidebar = () => {
-		setIsOpen(!isOpen);
-	};
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
 
-	return (
-		<>
-			<Router>
-				<Navbar toggleSidebar={toggleSidebar} />
-				<Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
-				<Seo />
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/projects" element={<ProjectsPage />} />
-					<Route path="/about" element={<AboutPage />} />
-					<Route path="/contact" element={<ContactPage />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
+    return (
+        <>
+            <Router>
+                <Navbar toggleSidebar={toggleSidebar}/>
+                <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar}/>
+                <Seo/>
+                <ScrollToTop/>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="/projects" element={<ProjectsPage/>}/>
+                    <Route path="/about" element={<AboutPage/>}/>
+                    <Route path="/contact" element={<ContactPage/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
 
-				<Footer />
-			</Router>
-		</>
-	);
+                <Footer/>
+            </Router>
+        </>
+    );
 }
 
 export default App;
