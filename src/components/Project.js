071f58a4bb157b34e6fa44accec2d5e56b93d77c
rameskum <rameskum.ms@outlook.com>
2022-04-18 15:53:50 +0530

    fix project indexing

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,47 +1,43 @@
 import React from 'react';
-import { FaGithub } from 'react-icons/fa';
-import { BsArrowUpRightSquare } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
+import {FaGithub} from 'react-icons/fa';
+import {BsArrowUpRightSquare} from 'react-icons/bs';
+import {Link} from 'react-router-dom';
 import defaultProjectImage from '../assets/images/rest-api.svg';
 
-const Project = ({ id, title, description, tags, code, demo, image }) => {
-	return (
-		<article className="project">
-			<div className="project-img">
-				<img alt={title} src={image || defaultProjectImage} />
-			</div>
-			<div className="project-info">
-				<span className="project-number">1.</span>
-				<Link className="project-slug" to={`/projects/`}>
-					<h3>{title}</h3>
-				</Link>
-				<p className="project-desc">{description}</p>
-				<div className="project-stack">
-					{tags.map((tag, idx) => {
-						return <span key={'tag-' + idx}>{tag}</span>;
-					})}
-				</div>
-				<div className="project-links">
-					<a
-						className="social-icon"
-						href={code}
-						target="_blank"
-						rel="noreferrer"
-					>
-						<FaGithub />
-					</a>
-					<a
-						className="social-icon"
-						href={demo}
-						target="_blank"
-						rel="noreferrer"
-					>
-						<BsArrowUpRightSquare />
-					</a>
-				</div>
-			</div>
-		</article>
-	);
+const Project = ({id, title, description, tags, code, demo, image}) => {
+    return (<article className="project">
+        <div className="project-img">
+            <img alt={title} src={image || defaultProjectImage}/>
+        </div>
+        <div className="project-info">
+            <span className="project-number">{id}.</span>
+            <Link className="project-slug" to={`/projects/`}>
+                <h3>{title}</h3>
+            </Link>
+            <p className="project-desc">{description}</p>
+            <div className="project-stack">
+                {tags.map((tag, idx) => {
+                    return <span key={'tag-' + idx}>{tag}</span>;
+                })}
+            </div>
+            <div className="project-links">
+                <a
+                    className="social-icon"
+                    href={code}
+                    target="_blank"
+                    rel="noreferrer">
+                    <FaGithub/>
+                </a>
+                <a
+                    className="social-icon"
+                    href={demo}
+                    target="_blank"
+                    rel="noreferrer">
+                    <BsArrowUpRightSquare/>
+                </a>
+            </div>
+        </div>
+    </article>);
 };
 
 export default Project;
