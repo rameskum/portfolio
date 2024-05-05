a5e3288dcfa8f6c7bc366f7f52f2e9626114f99d
rameskum <rameskum.ms@outlook.com>
2024-05-05 13:49:06 -0400

    adding additional projects

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,32 +1,73 @@
 const projects = [
-    {
-        id: 1,
-        title: 'Hello Dog',
-        description: 'I created this fun project completely with Javascript, CSS and HTML, where you can look at random dog images or see dog images by breed. The application also displays random dog facts. Check the demo link.',
-        tags: ['HTML', 'CSS', 'JAVASCRIPT'],
-        code: 'https://github.com/rameskum/hello-dog',
-        demo: 'https://rameskum.github.io/hello-dog/',
-        image: 'https://raw.githubusercontent.com/rameskum/hello-dog/master/src/res/hello-dog.png',
-        present: true
-    },
-    {
-        id: 2,
-        title: 'Breaking Bad',
-        description: 'This project was developed as learning project using Angular. The app lists `breaking bad` and `better call saul` characters with additional details, like their occupation, cause of death, season appearance, status (dead or alive in the season) and others.',
-        tags: ['Angular', 'RXJS', 'CSS'],
-        code: 'https://github.com/rameskum/breaking-bad',
-        demo: 'https://rameskum.github.io/breaking-bad/',
-        image: 'https://raw.githubusercontent.com/rameskum/breaking-bad/main/breaking-bad-app-ss.png',
-        present: true
-    },
-    {
-        id: 3,
-        title: 'Dog Facts',
-        description: 'This is the Dog API and it provides dog facts as a service.',
-        tags: ['JAVA', 'SPRING-BOOT'],
-        code: 'https://github.com/rameskum/dog-facts',
-        demo: 'https://rameskum.github.io/dog-facts/',
-    }
-]
+	{
+		id: 1,
+		title: 'E-Commerce Admin Dashboard',
+		description:
+			'E-Commerce Admin Dashboard is a web interface designed specifically for store administrators to manage and monitor their online business. It provides a centralized location to access key functionalities and gain insights into store performance.E-Commerce Admin Dashboard is a web interface designed specifically for store administrators to manage and monitor their online business. It provides a centralized location to access key functionalities and gain insights into store performance.',
+		tags: [
+			'Tailwind',
+			'Next.js',
+			'TypeScript',
+			'Stripe',
+			'Server-Side Rendering (SSR)',
+			'Client-Side Rendering (CSR)',
+			'Clerk Authentication',
+		],
+		code: 'https://github.com/rameskum/ecommerce-admin',
+		demo: 'https://ecommerce-admin.computerbyte.in',
+		image:
+			'https://raw.githubusercontent.com/rameskum/ecommerce-admin/main/Admin%20Dashboard.jpeg',
+		present: true,
+	},
+	{
+		id: 2,
+		title: 'E-Commerce Storefront',
+		description:
+			'This project leverages Next.js to build a user-friendly online store. It seamlessly integrates with a separate API ([Admin](https://github.com/rameskum/ecommerce-admin)) to fetch and display product data, offering a visually appealing and functional shopping experience.',
+		tags: [
+			'Tailwind',
+			'Next.js',
+			'TypeScript',
+			'Server-Side Rendering (SSR)',
+			'Client-Side Rendering (CSR)',
+		],
+		code: 'https://github.com/rameskum/ecommerce-store',
+		demo: 'https://ecommerce-store.computerbyte.in',
+		image:
+			'https://raw.githubusercontent.com/rameskum/ecommerce-store/main/Store.jpeg',
+		present: true,
+	},
+	{
+		id: 10,
+		title: 'Hello Dog',
+		description:
+			'I created this fun project completely with Javascript, CSS and HTML, where you can look at random dog images or see dog images by breed. The application also displays random dog facts. Check the demo link.',
+		tags: ['HTML', 'CSS', 'JAVASCRIPT'],
+		code: 'https://github.com/rameskum/hello-dog',
+		demo: 'https://rameskum.github.io/hello-dog/',
+		image:
+			'https://raw.githubusercontent.com/rameskum/hello-dog/master/src/res/hello-dog.png',
+		present: true,
+	},
+	{
+		id: 11,
+		title: 'Breaking Bad',
+		description:
+			'This project was developed as learning project using Angular. The app lists `breaking bad` and `better call saul` characters with additional details, like their occupation, cause of death, season appearance, status (dead or alive in the season) and others.',
+		tags: ['Angular', 'RXJS', 'CSS'],
+		code: 'https://github.com/rameskum/breaking-bad',
+		demo: 'https://rameskum.github.io/breaking-bad/',
+		image:
+			'https://raw.githubusercontent.com/rameskum/breaking-bad/main/breaking-bad-app-ss.png',
+	},
+	{
+		id: 12,
+		title: 'Dog Facts',
+		description: 'This is the Dog API and it provides dog facts as a service.',
+		tags: ['JAVA', 'SPRING-BOOT'],
+		code: 'https://github.com/rameskum/dog-facts',
+		demo: 'https://rameskum.github.io/dog-facts/',
+	},
+];
 
-export default projects
+export default projects;
