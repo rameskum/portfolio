a3e00f450a22b05c66cd217430243565eafe1028
rameskum <rameskum.ms@outlook.com>
2024-06-26 16:38:00 -0400

    ✨ Add mobile navigation to Header component.

---
+++ src/components/nav.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import Link from 'next/link';
+import React from 'react';
+import { cn } from '@/lib/utils';
+import { usePathname } from 'next/navigation';
+
+const links = [
+  {
+    name: 'Home',
+    href: '/',
+  },
+  {
+    name: 'About',
+    href: '/about',
+  },
+  {
+    name: 'Resume',
+    href: '/resume',
+  },
+  {
+    name: 'Work',
+    href: '/work',
+  },
+  {
+    name: 'Contact',
+    href: '/contact',
+  },
+];
+
+const Nav = () => {
+  const pathname = usePathname();
+  return (
+    <nav className="flex gap-8">
+      {links.map((link, index) => (
+        <Link
+          key={index}
+          href={link.href}
+          className={cn(
+            'font-medium capitalize transition-all hover:text-primary',
+            {
+              'border-b-2 border-accent text-primary': pathname === link.href,
+            },
+          )}
+        >
+          {link.name}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default Nav;
